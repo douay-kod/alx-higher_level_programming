2a8ba45965dd2607e2500c18455257f5e663e66a
douay-kod <douay.youssef@outlook.com>
2023-08-15 16:47:49 -0700

    ee

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
     """Print """
     from calculator_1 import add, sub, mul, div
 
-    a=10
-    b=5
+    a = 10
+    b = 5
 
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
