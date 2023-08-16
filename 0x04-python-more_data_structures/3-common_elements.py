e49ee053da8a07a741d1871420a1ac6531075417
douay-kod <douay.youssef@outlook.com>
2023-08-16 10:59:57 -0700

    ee

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    l = []
-    for i in set_1:
-        if i in set_2:
-            l.appen(i)
-    k = set(l)
-    return k
+    return set_1 ^ set_2
