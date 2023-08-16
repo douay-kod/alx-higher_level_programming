a3e28c7ddeb2085473db085e2bd779f430516f4d
douay-kod <douay.youssef@outlook.com>
2023-08-16 11:11:40 -0700

    ee

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    l = list(a_dictionary.key(s))
+    l.sort()
+    
+    for i in range(len(l)):
+        print("{}: {}".format(l[i],a_dictionary[i]))
